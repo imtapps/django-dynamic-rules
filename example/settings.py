d73115901c4f54d2ad55990e720f70da84a71f03
Aaron Madison <aaron.madison@theimtgroup.com>
2011-08-19 12:59:52 -0500

    added django-jenkins to example project so our ci environment can run nicely.

--- example/settings.py
+++ example/settings.py
@@ -39,3 +39,19 @@ INSTALLED_APPS = (
     'dynamic_rules',
     'sample',
 )
+
+try:
+    import django_jenkins
+    PROJECT_APPS = [app for app in INSTALLED_APPS if not app.startswith('django.contrib')]
+
+    INSTALLED_APPS = INSTALLED_APPS + ('django_jenkins',)
+    JENKINS_TASKS = (
+        'django_jenkins.tasks.django_tests',
+        'django_jenkins.tasks.run_pylint',
+        'django_jenkins.tasks.run_pep8',
+        'django_jenkins.tasks.run_pyflakes',
+        'django_jenkins.tasks.with_coverage',
+    )
+
+except ImportError:
+    pass
--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup, find_packages
+import os
+import re
+from distutils.core import Command, setup
+from setuptools import find_packages
 
 REQUIREMENTS = [
     'django',
@@ -9,35 +12,75 @@ REQUIREMENTS = [
 
 TEST_REQUIREMENTS = [
     'mock',
+    'django-jenkins',
+    'pep8',
+    'pyflakes',
 ]
 
 from dynamic_rules import VERSION
 
-setup(
-    name="django-dynamic-rules",
-    version=VERSION,
-    author="Matthew J. Morrison & Aaron Madison",
-    author_email="mattjmorrison@mattjmorrison.com",
-    description="Allows you to create dynamic rules related to a particular model",
-    long_description=open('README.txt', 'r').read(),
-    url="https://github.com/imtapps/django-dynamic-rules",
-    packages=find_packages(exclude=["example"]),
-    install_requires=REQUIREMENTS,
-    tests_require=TEST_REQUIREMENTS,
-    test_suite='runtests.runtests',
-    zip_safe=False,
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-    ],
-    dependency_links = (
-        'https://bitbucket.org/twanschik/django-autoload/get/tip.tar.gz#egg=django-autoload',
+def do_setup():
+    setup(
+        name="django-dynamic-rules",
+        version=VERSION,
+        author="Matthew J. Morrison & Aaron Madison",
+        author_email="mattjmorrison@mattjmorrison.com",
+        description="Allows you to create dynamic rules related to a particular model",
+        long_description=open('README.txt', 'r').read(),
+        url="https://github.com/imtapps/django-dynamic-rules",
+        packages=find_packages(exclude=["example"]),
+        install_requires=REQUIREMENTS,
+        tests_require=TEST_REQUIREMENTS,
+        test_suite='runtests.runtests',
+        zip_safe=False,
+        classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Environment :: Web Environment",
+            "Framework :: Django",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Application Frameworks",
+        ],
+        dependency_links = (
+            'https://bitbucket.org/twanschik/django-autoload/get/tip.tar.gz#egg=django-autoload',
+        ),
+        cmdclass={
+            'install_dev': InstallDependencies,
+        },
     )
-)
+
+
+class InstallDependencies(Command):
+    """
+    Command to install both develop dependencies and test dependencies.
+
+    Not sure why we can't find a built in command to do that already
+    in an accessible way.
+    """
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def get_test_dependencies(self):
+        """
+        replace all > or < in the dependencies so the system does not
+        try to redirect stdin or stdout from/to a file.
+        """
+        command_line_deps = ' '.join(TEST_REQUIREMENTS)
+        return re.sub(re.compile(r'([<>])'), r'\\\1', command_line_deps)
+
+    def run(self):
+        os.system("pip install ./")
+        os.system("pip install %s" % self.get_test_dependencies())
+
+
+if __name__ == '__main__':
+    do_setup()
